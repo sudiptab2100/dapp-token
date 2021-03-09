71632ff2bbcadb9bfe70310767bc65dbdc100134
Sudipta <sudiptab2100@gmail.com>
2021-03-09 12:39:31 +0530

    2. Smoke Test

--- test/DappToken.js
+++ test/DappToken.js
@@ -1,13 +1,13 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
-_deploy_contracts('DappToken', function(accounts){
+contract('DappToken', function(accounts) {
 
-    it('sets the totalsupply upon deployment', function() {
-        return DappToken.deployed().then(function(instance) {
-            tokenInstance = instance;
-            return tokenInstance.totalSupply();
-        }).then(function(totalSupply) {
-            assert.equal(totalSupply.toNumber(), 1000000, 'Set totalSupply to 1 mill')
-        })
-    })
+  it('sets the total supply upon deployment', function() {
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.totalSupply();
+    }).then(function(totalSupply) {
+      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+    });
+  });
 })
