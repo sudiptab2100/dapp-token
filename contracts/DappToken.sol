aa489ca554b39650a90250a7462733ac7bde9f49
Sudipta <sudiptab2100@gmail.com>
2021-03-10 18:28:15 +0530

    4. Delegated Transfe

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -45,4 +45,18 @@ contract DappToken {
         return true;
     }
 
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
 }
