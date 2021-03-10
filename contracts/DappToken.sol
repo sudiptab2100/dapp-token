e9eeca9a51ccab17e13594b24e0bcf4be39e40ab
Sudipta <sudiptab2100@gmail.com>
2021-03-10 09:17:15 +0530

    2. Transfer Token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,11 +2,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
-    // Constructor
-    // Set & Read the total no of tokens
+    string public name = 'DApp Token';
+    string public symbol = 'DAPP';
+    string public standard = 'DApp Token v1.0';
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
