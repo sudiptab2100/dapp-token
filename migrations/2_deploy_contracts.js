588678a08f8cb215c0636b2690888dc4c3d2a244
Sudipta <sudiptab2100@gmail.com>
2021-03-14 16:18:50 +0530

    4. token sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
+const tokenPrice = 1000000000000000; // in wei
 
 module.exports = function (deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function() {
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
